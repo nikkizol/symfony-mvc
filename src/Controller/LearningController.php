9bd0dfc4f3291b36083743930976d46cba33eb3b
MZ <nikita.zholkovskyi@gmail.com>
2020-11-06 12:25:54 +0100

    done

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -5,30 +5,46 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LearningController extends AbstractController
 {
-    /**
-     * @Route("/learning", name="learning")
-     */
-    public function aboutMe(): Response
+
+    public function aboutMe(SessionInterface $session): Response
     {
+        if ($session->get('name')) {
+            $name = $session->get('name');
+        } else {
+            return $this->forward('App\Controller\LearningController::showMyName');
+        }
         return $this->render('learning/aboutMe.html.twig', [
-            'name' => 'Nikita',
+            'name' => $name,
         ]);
     }
 
-    public function showMyName(): Response
+    public function showMyName(SessionInterface $session): Response
     {
+        if ($session->get('name')) {
+            $name = $session->get('name');
+        } else {
+            $name = 'unknown';
+        }
         return $this->render('learning/showMyName.html.twig', [
-            'name' => 'UNKNOWN',
+            'name' => $name,
         ]);
     }
 
-    public function changeMyName(): Response
+    public function changeMyName(SessionInterface $session): Response
     {
-        return $this->render('learning/changeMyName.html.twig', [
-            'name' => $_POST['new_name'],
-        ]);
+        if(isset($_POST['new_name'])) {
+            $session->set('name', $_POST['new_name']);
+            $this->render('learning/changeMyName.html.twig', [
+                'name' => $_POST['new_name'],
+            ]);
+            return $this->redirectToRoute('showMyName');
+        } return $this->redirectToRoute('showMyName');
+
     }
+
 }
